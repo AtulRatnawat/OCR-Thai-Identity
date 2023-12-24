3c13c36ca4ade1ad0417a0c2e12ee0955f44e784
AtulRatnawat <atulratnawat100@gmail.com>
2023-12-24 15:59:10 +0530

    Important Commit- To hide the MongoDB URI

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import Dropzone from 'react-dropzone';
 
-export default function Home() {
+const App = () => {
+  const [image, setImage] = useState(null);
+  const [labels, setLabels] = useState([]);
+
+  const handleDrop = async acceptedFiles => {
+    const formData = new FormData();
+    formData.append('image', acceptedFiles[0]);
+
+    try {
+      const response = await axios.post('http://localhost:5000/upload', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      setLabels(response.data.labels);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-        
+      <Dropzone onDrop={handleDrop}>
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()} style={{ border: '1px solid black', padding: '20px' }}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop an image here, or click to select one</p>
+          </div>
+        )}
+      </Dropzone>
+
+      {image && (
+        <div>
+          <h2>Image Preview:</h2>
+          <img src={image.preview} alt="Uploaded" style={{ width: '100%' }} />
+        </div>
+      )}
+
+      {labels.length > 0 && (
+        <div>
+          <h2>Labels:</h2>
+          <ul>
+            {labels.map((label, index) => (
+              <li key={index}>{label}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
+
+export default App;
