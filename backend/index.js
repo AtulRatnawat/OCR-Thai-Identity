c647fe933064d5b14f08d6ea89aeefab0029fd72
AtulRatnawat <atulratnawat100@gmail.com>
2023-12-25 23:11:03 +0530

    Ready to Host Frontend (Backend Hosted)

--- backend/index.js
+++ backend/index.js
@@ -16,18 +16,14 @@ const {storage} = new Storage();
 app.use(cors());
 app.use(express.json());
 
-const { ImageAnnotatorClient } = require('@google-cloud/vision');
-
-const client = new ImageAnnotatorClient();
-
 const multerStorage = multer.memoryStorage();
 const upload = multer({ storage: multerStorage });
 
 //Model
 const Entry = require('./models/entryModel');
 const ImgData = require('./models/ImgDataModel');
-const sizeOf = require('image-size');
 const router = express.Router();
+
 
 
 //API's
@@ -36,7 +32,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.post('/upload', upload.single('image'), async (req, res) => {
+app.post('/api/upload', upload.single('image'), async (req, res) => {
   try {
     // Process the image using the Google Vision API here
     // Use req.file.buffer to get the image buffer
@@ -48,10 +44,9 @@ app.post('/upload', upload.single('image'), async (req, res) => {
     res.json({ labels });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Errorrrrrrrrrrr' });
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
 
 
 
@@ -81,7 +76,6 @@ app.post('/api/entries', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
 
 // DELETE endpoint to delete an entry by ID
 app.delete('/api/entries/:identificationNumber', async (req, res) => {
@@ -155,7 +149,6 @@ app.post('/api/saveImage', upload.single('image'), async (req, res) => {
 });
 
 
-
 app.get('/api/getImageData/:id', async (req, res) => {
   try {
     const imageId = req.params.id;
@@ -177,8 +170,90 @@ app.get('/api/getImageData/:id', async (req, res) => {
 });
 
 
+/*------------------------------------------------------------------------------------------*/
 
-/*-------------------------------------------------------*/
+
+const CREDENTIALS = JSON.parse(JSON.stringify(
+  {
+    "type": "service_account",
+    "project_id": "omega-buckeye-341405",
+    "private_key_id": "308da733362a880c98545f2463b8a5cfeaa8f686",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCpTMhol/cEgcpg\nZPIG24kd7wEdHYaRa5qoW/ub4L8bydU1RvxSUvux8ziaq/lMk8RYAtOb0LYx/du4\ntHWFw/Z21d4Nt6SdcPlN4AooRIYGOOHd+LNnvPZOzZjxeoyY6OOftULL88O9TNsD\njERiV01gzmbn+OEsmP00e192KoWpB4vvsoc1WDSFF9ROG8Bc0Oxx/L7CAMYwa2KH\nL3LXzg9TOfGaxV+5c6drFu1+1D2qHnebDOllx0vq+7PGDcdy6TGezjSe5a87mTZ0\ni0ANykNj0QeI6P84i3Xm5O5Kfy1xUEXUYHOwRy2RnUynuvtamQCB7qcz4oFdSH7D\n7SrqeW0DAgMBAAECggEAA8DlDJSZ8GqkPwDd8NL7NkOvRaL68cxt8F+hs3IHKqmN\n5ZF+VKDsXaSj06IOc+kuyDi/0kuNToxvS6gMlvuiQj45nxo8NxVjIZve/cePJSIr\nt80kxR8GYvbh9jCeX2GCcm8FXRo5c7/5dxmsBnYjDAtncqss6t01d+1oT2lf70Bn\nUCvV7eHlsXUMkgYzXZ0hGiSRgm/fXbNBkYnj7EvtT5xoeOr7wkRg48KqK7nHOp1C\nkbX6emyVJU9+BGB61rii5CEeb8f0Bavh6fjzbrjjKkxPYgCcRMmMWdlvk5rA8NTz\nNgyvEp4+4EEMsUEpyouU/Zs6STVz+MnwZB8lmVT1SQKBgQDjqFCDBCPPZ+M/qGCn\nTfF/K6YVbb/2zUOX6y77IfqWeV0P58lgMelAk5Ko4c6yJpvVQpJTLP2b43P8MGgO\njRAhlb5LCz0Kll0cVCl0lPfxKVNRsgx6DO3m3E75y1RfB4RqCwKFKPWFgSpbGdze\neUZvfEX2rSehL7dJEKf49eXnLwKBgQC+YI0qH0OfMYMnwDMDPlspCthlfv/U7KXk\n5b5Q6ohPLRKao2ZLxj00QG0zGh+g8HvoHvICJ1Wn8ZIN8HjuxpQ94qbOG/V+1NJ2\neWFe/4xlDD8LPCuUmtJj+0uHbgTUegZYWdviUYaaDrNC5imwZnRMzfIAnVPzVvym\n+lrqVspibQKBgHYN9VpLjkpipByBs0VvDNmWh/S6yf4w6WlhYyaFs03Ot6yk1O+W\nUk/Tdw0us6H8lXjcocaSIR1ysNXJQFyzV/cikOCXflr8KGKlXi3/hie/9135B7FO\n/RtniWjNwP3ahxb6iJ8G43MMzSsa2Udg+l6bwiguGMvkU5aHFaPr2AGvAoGAeiQ6\nDX9XhEhg7Fz8naUhGZjys1Oi0bouyUe5LfL7F0lEuWACXrVGSULYjMPC7R+9b3SD\nMmyIISL04OqavB89tuK2wF/hxsjGaup/VVLBgYc034FwOHyIZUsx7ybgBG5xqbel\nQnOp1yd9TXub3aguD6TGEizeUq5JlNvk/ppGaJUCgYEAi1zr0NeWlaMp8frjXhyF\nGzkt5nl3hnADoyRB1XPA2VrG6G3BnfbDR8XnH4WAkeqgboJiNACnUV9D9OqbU996\nWXm0/SC65S5mfSKJ+4GPjGSR8OUQK6+Kx/Jj5tZJnVOKHYz4Dkr7spX+eRNi7o03\nVgsiH7u1SwMoEcHhs0zysSg=\n-----END PRIVATE KEY-----\n",
+      "client_email": "quala-project@omega-buckeye-341405.iam.gserviceaccount.com",
+      "client_id": "102692756936972640090",
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/quala-project%40omega-buckeye-341405.iam.gserviceaccount.com",
+      "universe_domain": "googleapis.com"
+  }
+  ))
+  const CONFIG = {
+    credentials: {
+      private_key: CREDENTIALS.private_key,
+      client_email: CREDENTIALS.client_email
+    }
+  }
+  
+  const { ImageAnnotatorClient } = require('@google-cloud/vision');
+  const vision = new ImageAnnotatorClient(CONFIG);
+  const client = new ImageAnnotatorClient(CONFIG);
+app.post('/api/fetch', upload.single('image'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+    console.log(req.file);
+
+    const { buffer } = req.file;
+    const base64EncodedImage = req.file.buffer.toString('base64');
+    
+    console.log(buffer);
+
+    const result = await vision.textDetection(base64EncodedImage);
+
+    console.log(result);
+
+
+    if (!result.fullTextAnnotation) {
+      console.log('Text annotation not found in the image');
+      return res.status(400).json({ error: 'Text annotation not found in the image' });
+    }
+
+    const stringResult = result.fullTextAnnotation.text;
+    console.log(stringResult);
+
+    const identificationNumber = searchBefore(stringResult, 'Identification Number');
+    const name = searchAfter(stringResult, 'Name');
+    const lastName = searchAfter(stringResult, 'Last name');
+    const birthDate = searchAfter(stringResult, 'Date of Birth');
+    const issueDate = searchBefore(stringResult, 'Date of Issue');
+    const expiryDate = searchBefore(stringResult, 'Date of Expiry');
+
+    const idCard = new IdCard({
+      identification_number: identificationNumber,
+      name: name,
+      last_name: lastName,
+      date_of_birth: birthDate,
+      date_of_issue: issueDate,
+      date_of_expiry: expiryDate,
+    });
+
+    await idCard.save();
+    res.json(idCard);
+  } catch (error) {
+    console.error('Error processing image details:');
+    res.status(500).json({ error: 'Internal Server Error', details: error.message });
+  }
+});
+
+
+
+
+
+
+/*-------------------------------------------------------------------------------------------*/
 
 // Connection Port
 
@@ -188,3 +263,50 @@ const server = app.listen(PORT, () => {
 });
 
 server.timeout = 60000; // Set timeout to 60 seconds
+
+
+
+function searchAfter(stringResult, search_String) {
+  let ans = "";
+  let index = stringResult.lastIndexOf(search_String);
+  index += search_String.length + 1;
+
+  while (stringResult[index] != '\n') {
+      ans += stringResult[index];
+      index++;
+  }
+  return ans;
+}
+
+
+function searchBefore(stringResult, searchString) {
+  let it = stringResult.lastIndexOf(searchString);
+  if(it==-1){
+      searchString = "Date of issue";
+      return searchBefore(stringResult,searchString);
+  }
+
+  it -= 2;   
+  let ascii_i = stringResult.charCodeAt(it) ;
+  let ascii_i1 = stringResult.charCodeAt(it-1) ;
+  // console.log("ascii_i",ascii_i);
+  // console.log("ascii_i1",ascii_i1);
+  
+  let ans = "",reversed="";
+  if ((ascii_i >= 48 && ascii_i <= 57)||(ascii_i1 >= 48 && ascii_i1 <= 57) ) {
+      while (stringResult[it] != '\n') {
+          ans += stringResult[it];
+          it--;
+      }
+      // console.log(ans);
+      reversed = ans.split('').reduce((acc, char) => char + acc, '');
+  }
+  else{
+      if(searchString==="Identification Number")
+      reversed = searchAfter(stringResult, "Thai National ID Card");
+      else{
+          reversed = searchAfter(stringResult,searchString);
+      }
+  }
+  return reversed;
+}
